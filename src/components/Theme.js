d4115448798123b29c4f0bd9f4773dc8cc91c7aa
ebectar <acebe424@gmail.com>
2018-05-28 12:49:42 -0600

    testing howler sound component

--- src/components/Theme.js
+++ src/components/Theme.js
@@ -1,18 +1,27 @@
 import React from 'react'
+import {Howl, Howler} from 'howler'
 
 class Theme extends React.Component {
-    render() {
-        return(
-            <select id="dropdown">
-                <option>Rain</option>
-                <option>Ocean</option>
-                <option>Jungle</option>
-                <option>Forest</option>
-                <option></option>
-                <option></option>
-            </select>
-        )
-    }
+
+playSound = (event) => {
+    let sound = new Howl({
+        src: ['./sounds/seagulls.wav']
+    })
+    sound.play()
+}
+
+render() {
+    return(
+        <select id="dropdown">
+            <option onClick={playSound}>Seagulls</option>
+            <option>Ocean</option>
+            <option>Jungle</option>
+            <option>Forest</option>
+            <option></option>
+            <option></option>
+        </select>
+    )
+}
 }
 
 export default Theme
